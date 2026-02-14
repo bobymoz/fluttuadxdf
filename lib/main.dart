ef4f5ddebf03617e5944677440dbd9a5c7df3797
bobymoz <itanhagira@gmail.com>
2026-02-14 20:56:02 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 
-// --- CONFIGURAÇÃO ---
-// COLE SUA CHAVE DO TMDB AQUI DENTRO DAS ASPAS
-const String tmdbApiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YzMxYjNhZWIyZTU5YWEyY2FmNzRjNzQ1Y2UxNTg4NyIsIm5iZiI6MTc2NjA3MTQ0MC40MDMsInN1YiI6IjY5NDQxYzkwNjRhOWQ4N2RiYzcwNTZlNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Pa2-x5hvsHeepw5f70ffBGX9L-4AaU7NBYtp8qSTQDA'; 
+// SUA CHAVE JÁ INSERIDA AQUI
+const String tmdbApiKey = '9c31b3aeb2e59aa2caf74c745ce15887'; 
 
 void main() {
   runApp(const JinocaApp());
@@ -15,61 +13,51 @@ void main() {
 
 class JinocaApp extends StatelessWidget {
   const JinocaApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'JINOCA',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: const Color(0xFF141414), // Preto Netflix
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-        ),
-        textTheme: GoogleFonts.interTextTheme(Theme.of(context).textTheme)
-            .apply(bodyColor: Colors.white, displayColor: Colors.white),
+        scaffoldBackgroundColor: const Color(0xFF000000),
       ),
       home: const HomeScreen(),
     );
   }
 }
 
-// --- TELA PRINCIPAL (LISTA DE FILMES) ---
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
-
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<dynamic> movies = [];
-  bool isLoading = true;
+  List movies = [];
+  bool loading = true;
 
   @override
   void initState() {
     super.initState();
-    fetchMovies();
+    loadData();
   }
 
-  Future<void> fetchMovies() async {
-    // Busca os filmes populares em PT-BR
-    final url = Uri.parse(
-        'https://api.themoviedb.org/3/movie/popular?api_key=$tmdbApiKey&language=pt-BR&page=1');
-    
+  Future<void> loadData() async {
     try {
-      final response = await http.get(url);
+      // Busca filmes populares em português usando sua chave
+      final response = await http.get(Uri.parse(
+          'https://api.themoviedb.org/3/movie/popular?api_key=$tmdbApiKey&language=pt-BR&page=1'));
+      
       if (response.statusCode == 200) {
-        final data = json.decode(response.body);
         setState(() {
-          movies = data['results'];
-          isLoading = false;
+          movies = json.decode(response.body)['results'];
+          loading = false;
         });
+      } else {
+        print("Erro na API: ${response.statusCode}");
       }
     } catch (e) {
-      print("Erro: $e");
-      setState(() => isLoading = false);
+      print("Erro de conexão: $e");
     }
   }
 
@@ -77,143 +65,82 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("JINOCA",
-            style: GoogleFonts.bebasNeue(color: Colors.red, fontSize: 30, letterSpacing: 2)),
+        title: Text("JINOCA", 
+          style: GoogleFonts.bebasNeue(color: Colors.red, fontSize: 35, letterSpacing: 2)),
         centerTitle: true,
+        backgroundColor: Colors.black,
       ),
-      body: isLoading
-          ? const Center(child: CircularProgressIndicator(color: Colors.red))
-          : GridView.builder(
-              padding: const EdgeInsets.all(10),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3, // 3 Colunas
-                childAspectRatio: 0.65, // Formato Cartaz
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-              ),
-              itemCount: movies.length,
-              itemBuilder: (context, index) {
-                return MovieCard(movie: movies[index]);
-              },
+      body: loading 
+        ? const Center(child: CircularProgressIndicator(color: Colors.red))
+        : GridView.builder(
+            padding: const EdgeInsets.all(10),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, 
+              childAspectRatio: 0.7, 
+              mainAxisSpacing: 10, 
+              crossAxisSpacing: 10
             ),
-    );
-  }
-}
-
-// --- CARD DO FILME ---
-class MovieCard extends StatelessWidget {
-  final dynamic movie;
-  const MovieCard({super.key, required this.movie});
-
-  @override
-  Widget build(BuildContext context) {
-    final posterPath = movie['poster_path'];
-    final imageUrl = "https://image.tmdb.org/t/p/w500$posterPath";
-    final title = movie['title'];
-    final id = movie['id'];
-
-    return GestureDetector(
-      onTap: () {
-        // Manda para o Player com a URL da SuperFlix montada
-        // A SuperFlix usa o ID do TMDB, então casa perfeitamente!
-        final superFlixUrl = "https://superflixapi.one/filme/$id";
-        
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => PlayerScreen(url: superFlixUrl),
-          ),
-        );
-      },
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(8),
-        child: Stack(
-          fit: StackFit.expand,
-          children: [
-            CachedNetworkImage(
-              imageUrl: imageUrl,
-              fit: BoxFit.cover,
-              placeholder: (context, url) => Container(color: Colors.grey[900]),
-              errorWidget: (context, url, error) => const Icon(Icons.error),
-            ),
-            // Sombra no título
-            Positioned(
-              bottom: 0, left: 0, right: 0,
-              child: Container(
-                color: Colors.black.withOpacity(0.7),
-                padding: const EdgeInsets.all(4),
-                child: Text(
-                  title,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
-                  textAlign: TextAlign.center,
+            itemCount: movies.length,
+            itemBuilder: (context, i) {
+              final m = movies[i];
+              final poster = "https://image.tmdb.org/t/p/w342${m['poster_path']}";
+              
+              return GestureDetector(
+                onTap: () => Navigator.push(
+                  context, 
+                  MaterialPageRoute(builder: (c) => PlayerScreen(id: m['id'], title: m['title']))
                 ),
-              ),
-            ),
-          ],
-        ),
-      ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: FadeInImage.assetNetwork(
+                    placeholder: 'assets/loading.gif', // se não tiver, use um Container preto
+                    image: poster,
+                    fit: BoxFit.cover,
+                    imageErrorBuilder: (c, e, s) => Container(color: Colors.grey[900], child: const Icon(Icons.movie)),
+                  ),
+                ),
+              );
+            },
+          ),
     );
   }
 }
 
-// --- PLAYER DE VÍDEO (COM BLOQUEADOR DE ANÚNCIOS) ---
 class PlayerScreen extends StatefulWidget {
-  final String url;
-  const PlayerScreen({super.key, required this.url});
-
+  final int id;
+  final String title;
+  const PlayerScreen({super.key, required this.id, required this.title});
   @override
   State<PlayerScreen> createState() => _PlayerScreenState();
 }
 
 class _PlayerScreenState extends State<PlayerScreen> {
-  late final WebViewController controller;
+  late final WebViewController _controller;
 
   @override
   void initState() {
     super.initState();
-    controller = WebViewController()
+    _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setBackgroundColor(Colors.black)
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onNavigationRequest: (NavigationRequest request) {
-            // A REGRA DE OURO:
-            // Se o link for do player (superflix), deixa passar.
-            if (request.url.contains('superflixapi.one')) {
-              return NavigationDecision.navigate;
-            }
-            // Se for qualquer outra coisa (popup, ads, bet), BLOQUEIA.
-            print("Pop-up bloqueado: ${request.url}");
-            return NavigationDecision.prevent;
-          },
-        ),
-      )
-      ..loadRequest(Uri.parse(widget.url));
+      ..setNavigationDelegate(NavigationDelegate(
+        onNavigationRequest: (req) {
+          // Bloqueador de Pop-ups
+          if (req.url.contains('superflixapi.one')) {
+            return NavigationDecision.navigate;
+          }
+          return NavigationDecision.prevent;
+        },
+      ))
+      ..loadRequest(Uri.parse("https://superflixapi.one/filme/${widget.id}"));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: SafeArea(
-        child: Stack(
-          children: [
-            WebViewWidget(controller: controller),
-            Positioned(
-              top: 10, left: 10,
-              child: CircleAvatar(
-                backgroundColor: Colors.black54,
-                child: IconButton(
-                  icon: const Icon(Icons.arrow_back, color: Colors.white),
-                  onPressed: () => Navigator.pop(context),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      appBar: AppBar(title: Text(widget.title, style: const TextStyle(fontSize: 16))),
+      backgroundColor: Colors.black, 
+      body: WebViewWidget(controller: _controller)
     );
   }
 }
