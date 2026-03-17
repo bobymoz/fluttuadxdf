2db40e852db0e47f8c2758f36e42ccf156286fbf
bobymoz <itanhagira@gmail.com>
2026-03-18 00:52:05 +0200

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1472,8 +1472,7 @@ class _PlayerScreenState extends State<PlayerScreen> {
                 ),
               ),
             ),
-        ],
-      ),
+          ),
 
         if (!isPlaying)
           Positioned(top: 10, left: 10, child: IconButton(icon: const Icon(Icons.arrow_back_ios, color: Colors.white), onPressed: () => Navigator.pop(context))),
