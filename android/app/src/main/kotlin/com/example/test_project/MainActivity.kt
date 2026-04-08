85ae5b680ea3c52321a18e54670cffbc24b050fd
bobymoz <itanhagira@gmail.com>
2026-04-08 17:22:38 +0200

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/test_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/test_project/MainActivity.kt
@@ -1,5 +1,31 @@
 package com.example.test_project
 
+import android.app.PictureInPictureParams
+import android.os.Build
+import android.util.Rational
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "cdcine/pip"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "enterPiP") {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    val params = PictureInPictureParams.Builder()
+                        .setAspectRatio(Rational(16, 9))
+                        .build()
+                    enterPictureInPictureMode(params)
+                    result.success(null)
+                } else {
+                    result.error("NOT_SUPPORTED", "PiP not supported", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+}
