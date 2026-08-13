a22db1ddca48684d7f8ff68cca7a37e5e55349c4
bobymoz <itanhagira@gmail.com>
2026-08-13 22:11:36 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -586,7 +586,7 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
   }
 }
 
-const String _appVersion = "2.2.1";
+const String _appVersion = "3.0.0";
 const String _versionUrl = "https://pastefy.app/FlTl6ufq/raw";
 class VersionGateScreen extends StatefulWidget { const VersionGateScreen({super.key}); @override State<VersionGateScreen> createState() => _VersionGateScreenState(); }
 class _VersionGateScreenState extends State<VersionGateScreen> {
